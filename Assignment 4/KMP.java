e7a5586d9baa5c75c515498332513c9fc5fa8694
Che-Chi (Jack) Liu <chechiliu@outlook.com>
2019-08-28 15:58:39 -0700

    Update KMP.java

--- Assignment 4/KMP.java
+++ Assignment 4/KMP.java
@@ -1,6 +1,13 @@
 //Che-Chi (Jack) Liu
 //V00850558
 
+//Input: A text file (the genome) and a pattern (the gene). Both consist entirely of characters from the alphabet {A, C, G, T}.
+//Output: If the text contains the pattern, the index of the first occurrence of the pattern in the text. Otherwise, the length of the text.
+
+//The implemented algorithm is O(N + M) the KMP algorithm. For each pattern, the running
+//time of the code is at most O(M + N), where N is the length of the text
+//being searched and M is the length of the pattern for which you are searching.
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
