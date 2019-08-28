139b999b33888c2243c722282f9c323bf0cd106b
Che-Chi (Jack) Liu <chechiliu@outlook.com>
2019-08-28 13:39:23 -0700

    Update QuickSelect.java

--- Assignment 1/QuickSelect.java
+++ Assignment 1/QuickSelect.java
@@ -1,139 +1,119 @@
-//Che-Chi Jack Liu
+//Che-Chi (Jack) Liu
 //V00850558
 
+//Quickselect with the median-of-medians pivot, where the medians are of groups of size 7.
+//	Input: An array A of n integers and an integer k in the set {1, 2, . . . , n}.
+//	Output: The kth smallest element of A.
 
 import java.util.*;
 
 public class QuickSelect {
-
    	public static int num_subgroup;
-
-   	public static int QuickSelect(int[] A, int k){
-    	return partition(A, grouping(A), k);
+	
+   	public static int QuickSelect(int[] A, int k) {
+    		return partition(A, grouping(A), k);
  	}
 
  	public static int grouping(int[] input) {
-    	num_subgroup = (int)Math.floor(input.length/7);
+    		num_subgroup = (int)Math.floor(input.length/7);
+		
+    		int[] output = new int[7];
+    		int[] median;
 
-    	int[] output = new int[7];
-    	int[] median;
-
-    	if(input.length % 7 != 0) {
-    		median = new int[num_subgroup + 1];  
-    	}
-    	else{
+    		if(input.length % 7 != 0) {
+    			median = new int[num_subgroup + 1];  
+    		}
+    		else {
    			median = new int[num_subgroup];
-    	}
-    	
-
-        int count=0;
-        int sub_count=1;		
-        int end=0;
-
-        
-        while(sub_count <= num_subgroup) {
+    		}
+    	  	
+		int count = 0;
+        	int sub_count = 1;		
+        	int end = 0;
+		
+       	 	while(sub_count <= num_subgroup) {
 			System.arraycopy(input, count, output, 0, 7);		
-
+			
 			Arrays.sort(output);
 			median[sub_count - 1] = output[3];
-
-			/*for(int i=0; i<output.length; i++) {
-				System.out.println(output[i]);
-			}*/
-
+			
 			count = count+7;
 			sub_count++;
-        }
+        	}
     
 		end = 6+((sub_count-2)*7);
 
 		int x = input.length - (7 * num_subgroup);
 		
    		if((input.length % 7) != 0) {
-     		output = new int[x];
-     		System.arraycopy(input, end+1, output, 0, x);
+     			output = new int[x];
+     			System.arraycopy(input, end+1, output, 0, x);
 
-     		Arrays.sort(output); 
+     			Arrays.sort(output);
      		 		
-     		if(((x + 1)%2)!= 0) { 
-     			median[sub_count-1] = output[(int)(Math.floor(x / 2))];  
+     			if(((x + 1)%2) != 0) {
+     				median[sub_count-1] = output[(int)(Math.floor(x / 2))];  
+     			}
+     			else {
+     				median[sub_count-1] = output[(int)(Math.floor((x + 1) / 2)) - 1];
+     			}
      		}
-     		else{
-     			median[sub_count-1] = output[ (int)(Math.floor((x + 1) / 2)) - 1 ];
+		
+     		if(median.length > 1) {
+     			return grouping(median);
      		}
-     	}
-
-     	
-     	//System.out.println(median[i]+"eeee");
-     	
-     	
-     	if(median.length > 1) {
-     		return grouping(median);
-     	}
-
-     	return median[0];
+		
+     		return median[0];
 	}
-
-
+	
 	public static int partition(int[] input, int mom, int k) {
-
 		ArrayList<Integer> left = new ArrayList<Integer>();
 		ArrayList<Integer> right = new ArrayList<Integer>();
 
 		Integer m = new Integer(mom);
 		int x;
-
-		for(int i=0; i < input.length; i++) {
+		
+		for(int i = 0; i < input.length; i++) {
 			if(input[i] <= mom) {
 				left.add(input[i]);
 			}
-			else{
+			else {
 				right.add(input[i]);
 			}
 		}
-
+		
 		if(left.contains(m)) {
 			left.remove(m);
 		}
-
+		
 		x = left.size() + 1;
+		
+		int[] lefta = new int[left.size()];
+		for(int i = 0; i < lefta.length; i++) {
+			lefta[i] = left.get(i).intValue();
+		}
 
-		int[] lefta;
-		lefta = toArray(left);
+		int[] righta = new int[right.size()];;
+		for(int i = 0; i < righta.length; i++) {
+			righta[i] = right.get(i).intValue();
+		}
 
-		int[] righta;
-		righta = toArray(right);
-
-		if( k < x && left.size() >= k) {
+		if(k < x && left.size() >= k) {
 			return QuickSelect(lefta, k);
 		}
-		else if (k > x && right.size() >= k) {
+		else if(k > x && right.size() >= k) {
 			return QuickSelect(righta, k);
 		}
-		else if(k > x && right.size() < k ) {
+		else if(k > x && right.size() < k) {
 			return QuickSelect(righta, k-x);
 		}
 
 		return mom;	
-
 	}
-
-	public static int[] toArray (List<Integer> input)	{
-    	int[] x = new int[input.size()];
-   		for (int i=0; i < x.length; i++) {
-        	x[i] = input.get(i).intValue();
-    	}
-   		return x;
-
-	}
-
-    public static void main(String[] args) {
-    	int[] A = {8, 7, 6, 5, 4, 3, 2};
-
-    	System.out.println("The median is " + QuickSelect(A, (A.length+1)/2));
-    	System.out.println(A.length);
-
-    	//System.out.println(grouping(A));
-		//System.out.println("The mom is "+QuickSelect(A, (A.length+1)/2));
+	
+   	public static void main(String[] args) {
+    		int[] A = {8, 7, 6, 5, 4, 3, 2};
+		
+    		System.out.println("The median is " + QuickSelect(A, (A.length+1)/2));
 	}
 }
